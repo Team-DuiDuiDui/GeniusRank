7c6f32a1b8a7c4e47956e5af919528087d6f5734
EST-NINE <143050853+EST-NINE@users.noreply.github.com>
2024-10-25 22:01:21 +0800

    refactor: ChatGLM 模型中的 Prompt 从缓存中拿取

--- backend/analyze-service/src/main/java/com/nine/project/analyze/controller/AiController.java
+++ backend/analyze-service/src/main/java/com/nine/project/analyze/controller/AiController.java
@@ -2,7 +2,9 @@ package com.nine.project.analyze.controller;
 
 
 import com.esotericsoftware.minlog.Log;
+import com.nine.project.analyze.constant.PromptConstant;
 import com.nine.project.analyze.dto.req.ChatReqDTO;
+import com.nine.project.analyze.service.PromptService;
 import com.nine.project.analyze.toolkit.AiManager;
 import com.nine.project.framework.result.Result;
 import com.nine.project.framework.web.Results;
@@ -16,7 +18,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
-import static com.nine.project.analyze.toolkit.AiManager.SYNC_PROMPT;
 
 
 /**
@@ -28,6 +29,8 @@ public class AiController {
 
     private final AiManager aiManager;
 
+    private final PromptService promptService;
+
     /**
      * 同步请求 Chat
      * @param chatReqDTO 聊天请求 DTO
@@ -35,7 +38,8 @@ public class AiController {
      */
     @PostMapping("/api/analyze/chat/sync")
     public Result<String> chatSync(@RequestBody ChatReqDTO chatReqDTO) {
-        return Results.success(aiManager.doSyncRequest(SYNC_PROMPT, chatReqDTO.getMessage()));
+        String syncPrompt = promptService.getPromptTextByType(PromptConstant.PromptType.SYNC.getCode());
+        return Results.success(aiManager.doSyncRequest(syncPrompt, chatReqDTO.getMessage()));
     }
 
     /**
@@ -46,7 +50,8 @@ public class AiController {
     @PostMapping("/api/analyze/chat/stream")
     public SseEmitter chatStream(@RequestBody ChatReqDTO chatReqDTO) {
         Log.info("hello");
-        Flowable<ModelData> modelDataFlowable = aiManager.doStreamRequest("你是一个开发者技术能力评估信息自动整理机器人，接下来我会给你一个 Github 用户的基础信息，比如在 GitHub 上有博客链接，甚至有一些用 GitHub 搭建的网站，也有一些在 GitHub 本身有账号相关介绍，你要结合这些基础信息来自动整理评估开发者技术能力", chatReqDTO.getMessage());
+        String streamPrompt = promptService.getPromptTextByType(PromptConstant.PromptType.STREAM.getCode());
+        Flowable<ModelData> modelDataFlowable = aiManager.doStreamRequest(streamPrompt, chatReqDTO.getMessage());
 
         SseEmitter sseEmitter = new SseEmitter();
         modelDataFlowable.observeOn(Schedulers.io())
