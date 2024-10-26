8eda379879f6aa0f8d1cc014275d079cf77b9b85
Heuluck Lu <89375068+Heuluck@users.noreply.github.com>
2024-10-27 01:58:14 +0800

    chores: re-add user-agent

--- frontend/app/utils/requests/ghapis/user.server.ts
+++ frontend/app/utils/requests/ghapis/user.server.ts
@@ -6,8 +6,8 @@ import { handleGithubReq } from '../request';
 
 export class GithubUserServerOnly extends GithubUser {
     private token: string;
-    private axiosInstanceWithToken: AxiosInstance
-    
+    private axiosInstanceWithToken: AxiosInstance;
+
     constructor(name: string, token: string) {
         super(name);
         this.token = token;
@@ -15,19 +15,22 @@ export class GithubUserServerOnly extends GithubUser {
             baseURL: 'https://api.github.com',
             headers: {
                 Authorization: `token ${this.token}`,
-            }
-        })
+                'User-Agent': 'Team-Duiduidui: Genius Rank',
+            },
+        });
     }
 
     async getUser(): Promise<User | undefined> {
         return await handleGithubReq<User, any>(
             () => this.axiosInstanceWithToken.get(`/users/${this.name}`),
-            (res) => userSchema.parseAsync(res.data))
+            (res) => userSchema.parseAsync(res.data)
+        );
     }
 
     async getFollowers(): Promise<UserAsFollowerArray | undefined> {
         return await handleGithubReq<UserAsFollowerArray, any>(
             () => this.axiosInstanceWithToken.get(`/users/${this.name}/followers`),
-            (res) => userAsFollowerArraySchema.parseAsync(res.data))
+            (res) => userAsFollowerArraySchema.parseAsync(res.data)
+        );
     }
 }
