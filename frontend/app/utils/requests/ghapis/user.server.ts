51605ae9b3b948d0a8c3fdb7b69c89ddd47f943e
NEKO_OwO <neeeeeeeeekooooooooo@gmail.com>
2024-10-26 15:24:36 +0800

    feat: 在 GithubUserServerOnly 类中添加自定义的 axios instance

--- frontend/app/utils/requests/ghapis/user.server.ts
+++ frontend/app/utils/requests/ghapis/user.server.ts
@@ -2,23 +2,31 @@ import { userEX } from '~/utils/EXAMPLES';
 import { userSchema } from './schema/user';
 import { User } from './typings/user';
 import { githubUser as GithubUser } from './user';
-import axios from 'axios';
+import axios, { AxiosInstance } from 'axios';
 
 export class GithubUserServerOnly extends GithubUser {
     private token: string;
+    private axiosInstance: AxiosInstance
     constructor(name: string, token: string) {
         super(name);
         this.token = token;
-    }
-    async getUser(): Promise<User> {
-        const res = await axios.get(`https://api.github.com/users/${this.name}`, {
+        this.axiosInstance = axios.create({
+            baseURL: 'https://api.github.com',
             headers: {
                 Authorization: `token ${this.token}`,
-                'User-Agent': 'Team-Duiduidui-GeniusRank',
-            },
-        });
+            }
+        })
+    }
+    async getUser(): Promise<User> {
+        const res = await this.axiosInstance.get(`/users/${this.name}`);
         const data = await userSchema.parseAsync(res.data);
         return data;
         return userEX;
+    }
+
+    async getFollowers(): Promise<User[]> {
+        const res = await this.axiosInstance.get(`/users/${this.name}/followers`);
+        const data = await userSchema.array().parseAsync(res.data);
+        return data;
     }
 }
