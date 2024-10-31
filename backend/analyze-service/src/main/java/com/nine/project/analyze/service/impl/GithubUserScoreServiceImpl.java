d0667950f0d9f67ae787e914cc283a84beaa4907
EST-NINE <143050853+EST-NINE@users.noreply.github.com>
2024-11-01 01:05:47 +0800

    feat: 为用户分数计算服务添加 Redis 缓存

--- backend/analyze-service/src/main/java/com/nine/project/analyze/service/impl/GithubUserScoreServiceImpl.java
+++ backend/analyze-service/src/main/java/com/nine/project/analyze/service/impl/GithubUserScoreServiceImpl.java
@@ -1,7 +1,9 @@
 package com.nine.project.analyze.service.impl;
 
 import cn.hutool.core.bean.BeanUtil;
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import cn.hutool.core.bean.copier.CopyOptions;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.nine.project.analyze.dao.entity.GithubUserScoreDO;
 import com.nine.project.analyze.dao.mapper.GithubUserScoreMapper;
@@ -9,13 +11,18 @@ import com.nine.project.analyze.dto.req.GithubUserScoreReqDTO;
 import com.nine.project.analyze.dto.resp.GithubUserScoreRespDTO;
 import com.nine.project.analyze.service.GithubUserScoreService;
 import com.nine.project.analyze.toolkit.GithubUserScoreCalculator;
-import com.nine.project.framework.exception.ClientException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import static com.nine.project.analyze.constant.RedisCacheConstant.*;
 
 
 /**
@@ -26,43 +33,78 @@ import java.time.format.DateTimeFormatter;
 @RequiredArgsConstructor
 public class GithubUserScoreServiceImpl extends ServiceImpl<GithubUserScoreMapper, GithubUserScoreDO> implements GithubUserScoreService {
 
+    private final StringRedisTemplate stringRedisTemplate;
+
     @Override
     public GithubUserScoreRespDTO getGithubUserScore(String githubUserId) {
+        String cacheKey = USER_SCORE_KEY + githubUserId;
 
-        GithubUserScoreDO userScoreDO = this.getOne(new QueryWrapper<GithubUserScoreDO>().eq("github_user_id", githubUserId));
-        if (userScoreDO == null) {
-            throw new ClientException("github_user_id NOT FIND");
+        // 从缓存中获取
+        Map<Object, Object> cachedData = stringRedisTemplate.opsForHash().entries(cacheKey);
+        if (!cachedData.isEmpty()) {
+            return BeanUtil.fillBeanWithMap(cachedData, GithubUserScoreRespDTO.builder().build(), true);
         }
 
-        return BeanUtil.copyProperties(userScoreDO, GithubUserScoreRespDTO.class);
+        // 如果缓存中不存在，从数据库中查询
+        LambdaQueryWrapper<GithubUserScoreDO> queryWrapper = Wrappers.lambdaQuery(GithubUserScoreDO.class)
+                .eq(GithubUserScoreDO::getGithubUserId, githubUserId)
+                .eq(GithubUserScoreDO::getDelFlag, 0);
+
+        GithubUserScoreDO userScoreDO = this.getOne(queryWrapper);
+
+        // 存入缓存
+        GithubUserScoreRespDTO githubUserScoreRespDTO = BeanUtil.copyProperties(userScoreDO, GithubUserScoreRespDTO.class);
+
+        return send2Cache(cacheKey, githubUserScoreRespDTO);
     }
 
     @Override
     public GithubUserScoreRespDTO generateScore(GithubUserScoreReqDTO requestParams) {
+        String cacheKey = USER_SCORE_KEY + requestParams.getUser().getGithub_user_id();
+
+        // 使用查询计数来判断记录是否存在
+        LambdaQueryWrapper<GithubUserScoreDO> queryWrapper = Wrappers.lambdaQuery(GithubUserScoreDO.class)
+                .eq(GithubUserScoreDO::getGithubUserId, requestParams.getUser().getGithub_user_id())
+                .eq(GithubUserScoreDO::getDelFlag, 0);
+        long count = this.count(queryWrapper);
+        boolean existsInDatabase = count > 0;
+
         // 计算分数
         GithubUserScoreRespDTO scores = GithubUserScoreCalculator.calculate(requestParams);
 
-        // 持久化用户分数
-        GithubUserScoreDO userScoreDO = GithubUserScoreDO.builder()
-                .githubUserId(requestParams.getUser().getGithub_user_id())
-                .totalScore(scores.getTotalScore())
-                .userScore(scores.getUserScore())
-                .reposScore(scores.getReposScore())
-                .prsScore(scores.getPrsScore())
-                .issuesScore(scores.getIssuesScore())
-                .build();
-        boolean save = this.save(userScoreDO);
-        if (!save) {
-            throw new ClientException("USER SCORE PERSISTENCE FAILED");
+        // 封装持久化数据
+        GithubUserScoreDO userScoreDO = BeanUtil.copyProperties(scores, GithubUserScoreDO.class);
+        userScoreDO.setGithubUserId(requestParams.getUser().getGithub_user_id());
+
+        // 如果存在，则更新。如果不存在，则添加
+        if (existsInDatabase) {
+            this.update(userScoreDO, queryWrapper);
+        } else{
+            this.save(userScoreDO);
         }
 
         // 封装响应数据
-        GithubUserScoreRespDTO githubUserScoreRespDTO = BeanUtil.copyProperties(userScoreDO, GithubUserScoreRespDTO.class);
-
+        GithubUserScoreRespDTO respDTO = BeanUtil.copyProperties(userScoreDO, GithubUserScoreRespDTO.class);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
-        String now = LocalDateTime.now().format(formatter);
-        githubUserScoreRespDTO.setUpdateTime(LocalDateTime.parse(now, formatter));
+        respDTO.setUpdateTime(LocalDateTime.parse(LocalDateTime.now().format(formatter), formatter));
 
-        return githubUserScoreRespDTO;
+        // 存入缓存
+        return send2Cache(cacheKey, respDTO);
+    }
+
+    /**
+     * 存入 Redis 缓存
+     * @param key  Redis 键
+     * @param respDTO  Redis 值
+     * @return 响应数据
+     */
+    private GithubUserScoreRespDTO send2Cache(String key, GithubUserScoreRespDTO respDTO) {
+        Map<String, Object> map = BeanUtil.beanToMap(respDTO , new HashMap<>(), CopyOptions.create()
+                .setIgnoreNullValue(true)
+                .setFieldValueEditor((fieldName, fieldValue) -> fieldValue != null ? fieldValue.toString() : null));
+
+        stringRedisTemplate.opsForHash().putAll(key, map);
+        stringRedisTemplate.expire(key, USER_SCORE_EXPIRE_TIME, TimeUnit.SECONDS);
+        return respDTO;
     }
 }
