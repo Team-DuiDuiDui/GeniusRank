fc6f3e4be60b519c0d300f0e8dd1bb6725f007cd
Heuluck Lu <89375068+Heuluck@users.noreply.github.com>
2024-10-31 18:38:08 +0800

    feat: 将侧边栏抽象为组件

--- frontend/app/root.tsx
+++ frontend/app/root.tsx
@@ -1,38 +1,19 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 import { LoaderFunctionArgs, json } from '@remix-run/cloudflare';
-import {
-    Links,
-    Meta,
-    Outlet,
-    Scripts,
-    ScrollRestoration,
-    useFetcher,
-    useLoaderData,
-    useNavigate,
-    useRouteLoaderData,
-} from '@remix-run/react';
+import { Links, Meta, Outlet, Scripts, ScrollRestoration, useLoaderData, useRouteLoaderData } from '@remix-run/react';
 import i18nServer, { localeCookie } from './modules/i18n.server';
 import { useChangeLanguage } from 'remix-i18next/react';
 import { Toaster } from 'react-hot-toast';
-import {
-    ActionIcon,
-    Avatar,
-    Button,
-    ColorSchemeScript,
-    Divider,
-    Drawer,
-    MantineProvider,
-    SegmentedControl,
-} from '@mantine/core';
+import { ActionIcon, ColorSchemeScript, MantineProvider } from '@mantine/core';
 import { ClientOnly } from 'remix-utils/client-only';
 import '@mantine/core/styles.css';
 import '@mantine/charts/styles.css';
 import './tailwind.css';
-import { GithubFilled, MenuOutlined } from '@ant-design/icons';
 import { useDisclosure } from '@mantine/hooks';
-import { useTranslation } from 'react-i18next';
 import { user } from './user-cookie';
+import SettingDrawer from './components/drawer';
+import { MenuOutlined } from '@ant-design/icons';
 
 export const handle = { i18n: ['translation'] };
 
@@ -60,72 +41,23 @@ export function Layout({ children }: { children: React.ReactNode }) {
 export default function App() {
     const loaderData = useLoaderData<typeof loader>();
     const { locale, client_id, userAvatar, userLogin, username, userEmail } = loaderData;
-    const navigate = useNavigate();
-    const { t, i18n } = useTranslation();
-    const fetcher = useFetcher();
-    const [opened, { open, close }] = useDisclosure(false);
-    const [logOut, { toggle: toggleLogOut }] = useDisclosure();
+    const [drawerOpened, { open: openDrawer, close: closeDrawer }] = useDisclosure(false);
     useChangeLanguage(locale);
     return (
         <>
             <ClientOnly>{() => <Toaster />}</ClientOnly>
-            <ActionIcon variant="default" size="xl" className="z-40 m-10 absolute" onClick={open}>
+            <ActionIcon variant="default" size="xl" className="z-40 m-10 absolute" onClick={openDrawer}>
                 <MenuOutlined className="text-2xl text-gray-500" />
             </ActionIcon>
-            <Drawer opened={opened} onClose={close}>
-                <div className="flex flex-col gap-5 h-full">
-                    <div className={`flex flex-row justify-center gap-5`}>
-                        <Avatar size="lg" src={userAvatar ?? undefined} />
-                        <div className="flex flex-col justify-center">
-                            <p className="text-lg font-bold">{username ?? userLogin ?? t('drawer.anonymous')}</p>
-                            {userLogin && <p className="text-sm text-gray-500">{userEmail ?? userLogin}</p>}
-                        </div>
-                    </div>
-                    <SegmentedControl
-                        className="mt-auto"
-                        defaultValue={i18n.language}
-                        onChange={(value) => {
-                            fetcher.submit({ lng: value }, { method: 'get' });
-                            i18n.changeLanguage(value);
-                        }}
-                        data={[
-                            {
-                                label: '中文',
-                                value: 'zh',
-                            },
-                            {
-                                label: 'English',
-                                value: 'en',
-                            },
-                        ]}
-                    />
-                    <Divider label={userLogin ? t('drawer.logout') : t('drawer.login')} labelPosition="center" />
-                    {userLogin ? (
-                        <Button
-                            variant="filled"
-                            color="red"
-                            type="submit"
-                            loading={logOut}
-                            onClick={async () => {
-                                toggleLogOut();
-                                await fetch('/logout', { method: 'post' });
-                                navigate('.', { replace: true });
-                            }}>
-                            {t('drawer.logout')}
-                        </Button>
-                    ) : (
-                        <Button
-                            variant="default"
-                            component="a"
-                            href={`https://github.com/login/oauth/authorize/?client_id=${client_id}&scope=user:email,read:user`}>
-                            <div className="flex gap-2 items-center justify-center text-base">
-                                <GithubFilled />
-                                {t('drawer.login_github')}
-                            </div>
-                        </Button>
-                    )}
-                </div>
-            </Drawer>
+            <SettingDrawer
+                opened={drawerOpened}
+                close={closeDrawer}
+                userLogin={userLogin}
+                userEmail={userEmail}
+                userAvatar={userAvatar}
+                username={username}
+                client_id={client_id}
+            />
             <Outlet />
         </>
     );
