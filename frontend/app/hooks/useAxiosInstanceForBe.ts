873448ae3124193d5c4e159dc1f3627f445d50e2
Heuluck Lu <89375068+Heuluck@users.noreply.github.com>
2024-11-03 01:41:40 +0800

    feat: 不再使用 localStorage

--- frontend/app/hooks/useAxiosInstanceForBe.ts
+++ frontend/app/hooks/useAxiosInstanceForBe.ts
@@ -5,7 +5,7 @@ import { AxiosInstanceForBe, createInstanceForBe } from '~/api/instance';
  * 用于【 闭包 】创建后端 axios 实例，防止重复创建浪费内存
  * @returns 后端 axios 实例
  */
-const useAxiosInstanceForBe = (baseUrl: string): AxiosInstanceForBe | undefined => {
+const useAxiosInstanceForBe = (baseUrl: string, token?: string): AxiosInstanceForBe | undefined => {
     const currentToken = useRef<string | null>(null);
     const [isClient, setIsClient] = useState<boolean>(false);
     const [instance, setInstance] = useState<() => AxiosInstanceForBe | undefined>();
@@ -18,29 +18,11 @@ const useAxiosInstanceForBe = (baseUrl: string): AxiosInstanceForBe | undefined 
     useEffect(() => {
         // 仅在客户端运行
         if (!isClient) return;
-
-        const token = localStorage.getItem('token');
         if (token && token !== currentToken.current) {
             setInstance(() => () => createInstanceForBe(baseUrl, token));
             currentToken.current = token;
-        }
-
-        const handleStorageChange = (event: StorageEvent) => {
-            if (event.key === 'token') {
-                const newToken = event.newValue;
-                if (newToken && newToken !== currentToken.current) {
-                    setInstance(() => () => createInstanceForBe(baseUrl, newToken));
-                    currentToken.current = newToken;
-                }
-            }
-        };
-
-        window.addEventListener('storage', handleStorageChange);
-
-        return () => {
-            window.removeEventListener('storage', handleStorageChange);
-        };
-    }, [baseUrl, isClient]);
+        } else setInstance(() => () => createInstanceForBe(baseUrl));
+    }, [baseUrl, isClient, token]);
 
     return instance?.();
 };
