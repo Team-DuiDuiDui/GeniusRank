1782e05c40958a38088fafacb99b553e08d2f418
EST-NINE <143050853+EST-NINE@users.noreply.github.com>
2024-11-03 23:18:53 +0800

    perf: 使用 RocketMQ 异步持久化用户分数和开发者领域

---
+++ backend/analyze-service/src/main/java/com/nine/project/analyze/constant/RocketMQConstant.java
@@ -0,0 +1,22 @@
+package com.nine.project.analyze.constant;
+
+/**
+ * RocketMQ 常量
+ */
+public final class RocketMQConstant {
+
+    /**
+     * 服务消费者组 Key
+     */
+    public static final String CONSUMER_GROUP = "common-message_cg";
+
+    /**
+     * 服务 Topic Key
+     */
+    public static final String  TOPIC_KEY = "common-message_topic";
+
+    /**
+     * 开发者领域服务 Tag Key2
+     */
+    public static final String DETAILED_DEVELOP_TYPE_TAG = "common-message_detail_develop_type_tag";
+}
