ee6d3a6898b6c7bffec6c75cf04c07ce9c0a5463
Heuluck Lu <89375068+Heuluck@users.noreply.github.com>
2024-11-05 13:22:02 +0800

    feat: 首页

--- frontend/app/routes/_index.tsx
+++ frontend/app/routes/_index.tsx
@@ -1,4 +1,6 @@
+import { Button } from '@mantine/core';
 import { json, LoaderFunctionArgs, type MetaFunction } from '@remix-run/cloudflare';
+import { useTranslation } from 'react-i18next';
 import i18nServer from '~/modules/i18n.server';
 export const meta: MetaFunction<typeof loader> = ({ data }) => {
     return [{ title: data?.title }, { name: 'description', content: data?.description }];
@@ -10,5 +12,16 @@ export async function loader({ request }: LoaderFunctionArgs) {
 }
 
 export default function Index() {
-    return <></>;
+    const { t } = useTranslation();
+    return (
+        <>
+            <div className="px-8 py-12 bg-blue-400/70 text-white flex flex-col justify-center items-center gap-2">
+                <h1 className="text-6xl font-bold">Genius Rank</h1>
+                <h2 className="text-2xl">{t('description')}</h2>
+                <Button variant="default" size="md" className="mt-4">
+                    {t('lookup_docs')}
+                </Button>
+            </div>
+        </>
+    );
 }
