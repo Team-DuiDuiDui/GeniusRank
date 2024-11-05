51a0791af201b969bad630c022cdc397eeedeba9
NEKO_OwO <neeeeeeeeekooooooooo@gmail.com>
2024-11-05 22:58:35 +0800

    feat: better prompt

--- frontend/app/api/chat.ts
+++ frontend/app/api/chat.ts
@@ -38,7 +38,7 @@ export const syncChatForNationFromReadme = async (data: string, beInstance: Axio
     const message = `${data}
     这是一个 README 文件中的内容，里面可能包含了作者的国家信息。
     请你从这里面找寻作者明确声明了自己属于某个地区的信息，并且告诉我这个地区属于哪个国家。
-    注意，一定要从作者明确声明的地区，不要猜测。如果作者未声明地区，在 nationName 字段中返回空字符串即可。
+    注意，一定要从作者明确声明的地区，不要猜测。如果作者未声明地区，在 nationISO 字段中返回空字符串即可。
     你的输出内容需要是如下 json 格式的文本，格式如下（[]中是需要你判断的内容）。**注意：你的回答需要且只需要包含下面格式的 json 内容即可，不要有任何多余内容**
     {
         "nationName": [国家的英文全称],
@@ -46,6 +46,23 @@ export const syncChatForNationFromReadme = async (data: string, beInstance: Axio
     }
     `
     return JSON.parse(parseStringToJSONfy(await syncChat(message, beInstance)))
+}
+
+export const syncChatForNationFromGLM = async (userName: string, beInstance: AxiosInstanceForBe): Promise<NationData> => {
+    const message = `
+    按照你的数据库或者从互联网的公开信息来看，你知道 ${userName} 这个 github 用户对应的现实当中的真人吗？
+    他是一个粉丝数量很多的公众人物，如果经过合理搜索你是一定知道的，我现在正在对这些有名的人进行数据统计，请你协助我，这并不涉及到侵犯个人隐私之类。
+    你如果知道的话，请告诉我这个人来自哪个国家。如果你并不知道或者不确定，请再在返回值中的 nationISO 字段返回空字符串即可。
+    如果他是混血儿，请告诉我他最主要的国籍。
+    如果他是移居者或者双重国籍，请告诉我出生的时候的国家，比如 linus torvalds 在 wiki 上面是 Finnish-American 就取他的出生地 Finnish。
+    {
+        "nationName": [国家的英文全称],
+        "nationISO": [国家对应的 ISO 简写]
+    }
+    `
+    const result = await syncChat(message, beInstance)
+    console.log(result)
+    return JSON.parse(parseStringToJSONfy(result))
 }
 
 /**
