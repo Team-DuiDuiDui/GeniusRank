049872d68c53757b1c572363ba63f0d4eb565040
Heuluck Lu <89375068+Heuluck@users.noreply.github.com>
2024-11-06 01:37:13 +0800

    feat: 排名筛选器

--- frontend/app/routes/ranking.tsx
+++ frontend/app/routes/ranking.tsx
@@ -1,6 +1,9 @@
+import { Select } from '@mantine/core';
 import { json, LoaderFunctionArgs, MetaFunction } from '@remix-run/cloudflare';
-import { useLoaderData } from '@remix-run/react';
+import { useLoaderData, useSearchParams } from '@remix-run/react';
+import { useTranslation } from 'react-i18next';
 import { createInstanceForBe } from '~/api/instance';
+import { ScoreRankResp } from '~/api/typings/beRes';
 import { UserAccordion, UserCard } from '~/components/ranking/card';
 import i18nServer from '~/modules/i18n.server';
 import { getRankings } from '~/utils/requests/ranking';
@@ -13,7 +16,13 @@ export async function loader({ request, context }: LoaderFunctionArgs) {
     try {
         const res = await getRankings(beInstance, nation, type, 20);
         return json(
-            { ranking: res.data, title: `${t('ranking.title')} | Genius Rank`, description: t('ranking.description') },
+            {
+                ranking: res.data,
+                title: `${t('ranking.title')} | Genius Rank`,
+                description: t('ranking.description'),
+                type,
+                nation,
+            },
             { status: 200 }
         );
     } catch (e) {
@@ -40,18 +49,48 @@ export const meta: MetaFunction<typeof loader> = ({ data }) => {
 
 export default function Ranking() {
     const loaderData = useLoaderData<typeof loader>();
+    const [searchParams, setSearchParams] = useSearchParams();
+    const { t } = useTranslation();
+    const rankingData: ScoreRankResp[] = JSON.parse(JSON.stringify(loaderData.ranking.data));
+    const splicedData = rankingData.splice(0, Math.ceil(loaderData.ranking.data.length / 2));
     return (
-        <div>
-            <div className="flex flex-row justify-around my-12">
+        <div className="my-12 mx-32">
+            <div className="flex flex-row justify-start gap-8">
+                <Select
+                    onChange={(value) => {
+                        setSearchParams({
+                            ...(value && { type: value ?? '' }),
+                            ...(searchParams.get('nation') && { nation: searchParams.get('nation') ?? '' }),
+                        });
+                    }}
+                    defaultValue={loaderData.type}
+                    placeholder={t('ranking.type')}
+                    data={['Java', 'JavaScript', 'Python', 'Golang', 'TypeScript', 'Rust', 'C']}
+                    searchable
+                    clearable
+                />
+                <Select
+                    onChange={(value) => {
+                        setSearchParams({
+                            ...(value !== null && { nation: value ?? '' }),
+                            ...(searchParams.get('nation') !== null && { type: searchParams.get('type') ?? '' }),
+                        });
+                    }}
+                    defaultValue={loaderData.type}
+                    placeholder={t('ranking.nation')}
+                    data={['China', 'US']}
+                    searchable
+                    clearable
+                />
+            </div>
+            <div className="flex flex-row justify-between mt-8">
                 <UserAccordion>
-                    {loaderData.ranking.data
-                        .slice(0, Math.ceil(loaderData.ranking.data.length / 2))
-                        .map((item, index) => (
-                            <UserCard key={index} userInfo={item} score={item} />
-                        ))}
+                    {splicedData.map((item, index) => (
+                        <UserCard key={index} userInfo={item} score={item} />
+                    ))}
                 </UserAccordion>
                 <UserAccordion>
-                    {loaderData.ranking.data.slice(Math.ceil(loaderData.ranking.data.length / 2)).map((item, index) => (
+                    {rankingData.map((item, index) => (
                         <UserCard key={index} userInfo={item} score={item} />
                     ))}
                 </UserAccordion>
