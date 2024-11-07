ccba9f59b7852b7b5477f7e6fac51399fd1fbbf7
NEKO_OwO <neeeeeeeeekooooooooo@gmail.com>
2024-11-07 21:54:30 +0800

    fix: 无缝主页动画

--- frontend/app/routes/_index.tsx
+++ frontend/app/routes/_index.tsx
@@ -86,7 +86,7 @@ export default function Index() {
                                 userInfo={item}
                                 score={item}
                                 style={{
-                                    animation: `${lineIndex % 2 ? 'scrollLineO' : 'scrollLineI'} 90s linear infinite ${lineIndex % 2 ? 'reverse' : ''
+                                    animation: `${lineIndex % 2 ? 'scrollLineO' : 'scrollLineI'} 60s linear infinite ${lineIndex % 2 ? 'reverse' : ''
                                         }`,
                                 }}
                                 disabledChevron
