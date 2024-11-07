9233691e4f6e7baace7f67f6aee7577ef472a388
Heuluck Lu <89375068+Heuluck@users.noreply.github.com>
2024-11-07 23:30:39 +0800

    fix: 修复 Markdown 错误

--- frontend/app/components/userinfo/score.tsx
+++ frontend/app/components/userinfo/score.tsx
@@ -74,10 +74,10 @@ const UserScore: React.FC<userRepositoriesProps> = ({ data, user }) => {
                             <input
                                 className="px-4 py-2 focus-visible:outline-none w-80"
                                 readOnly
-                                value={`[![Genius Rank](https://geniusrank.heuluck.top/card/${data.login})](https://geniusrank.heuluck.top/)]`}
+                                value={`[![Genius Rank](https://geniusrank.heuluck.top/card/${data.login})](https://geniusrank.heuluck.top/)`}
                             />
                             <CopyButton
-                                value={`[![Genius Rank](https://geniusrank.heuluck.top/card/${data.login})](https://geniusrank.heuluck.top/)]`}>
+                                value={`[![Genius Rank](https://geniusrank.heuluck.top/card/${data.login})](https://geniusrank.heuluck.top/)`}>
                                 {({ copied, copy }) => (
                                     <Tooltip label={copied ? t('user.score.copied') : t('user.score.copy')}>
                                         <button
