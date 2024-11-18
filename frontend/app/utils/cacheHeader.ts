7e2feab1f281a5cccfac8437234d8ad5d8f04b5a
NEKO_OwO <neeeeeeeeekooooooooo@gmail.com>
2024-11-18 23:37:54 +0800

    hotfix

--- frontend/app/utils/cacheHeader.ts
+++ frontend/app/utils/cacheHeader.ts
@@ -2,6 +2,7 @@ export const cacheHeader = (clientSecs?: number, serverSecs?: number) => {
     return {
         headers: {
             'Cache-Control': `public, ${clientSecs ? `max-age=${clientSecs}` : ""}, ${serverSecs ? `s-maxage=${serverSecs}` : ""}, stale-while-revalidate=3600, stale-if-error=3600`,
+            'Content-Security-Policy': "default-src 'self'; script-src 'self'; style-src 'self'; img-src 'self';"
         }
     }
 }
