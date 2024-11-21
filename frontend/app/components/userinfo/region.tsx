e06d5664d0d19b5deb6ae3a5541d58c1b10ef1af
Heuluck Lu <89375068+Heuluck@users.noreply.github.com>
2024-11-21 21:16:18 +0800

    hotfix

--- frontend/app/components/userinfo/region.tsx
+++ frontend/app/components/userinfo/region.tsx
@@ -38,7 +38,7 @@ const UserNation: React.FC<NationCardProps> = ({
     const isCN = nationISO === "CN";
     const { colorScheme } = useMantineColorScheme() as unknown as { colorScheme: string };
     const [loading, setLoading] = useState(isStillHim);
-    const infoColor = confidence ? RGBToHex(interpolateColors(colorScheme === "dark" ? ["#f87171", "#e7e5e4"].map(hexToRgb) : ["#991b1b", "#f5f5f4"].map(hexToRgb), confidence))
+    const infoColor = confidence ? RGBToHex(interpolateColors(colorScheme === "dark" ? ["#f87171", "#e7e5e4"].map(hexToRgb) : ["#991b1b", "#222222"].map(hexToRgb), confidence))
         : "";
     console.log(infoColor)
     useEffect(() => {
